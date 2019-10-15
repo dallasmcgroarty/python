3e6136b1993a44d1cba608f888ba8a7f565b2a9d
dallasmcgroarty <dallas.mcgoarty@gmail.com>
2019-10-15 12:32:52 -0700

    simple python funcs

--- pract.py
+++ pract.py
@@ -16,13 +16,13 @@ def temperature(temp):
     elif temp <= 100:
         print('hot')
 
-print(temperature(30))
+print('temp--> ', temperature(30))
 
 string1 = '1,2,3,4,5,6,7'
 
 string1 = string1.replace(',','')
 
-print(string1)
+print('no commas --> ', string1)
 print(type(string1))
 
 sum1 = 0
@@ -62,7 +62,7 @@ list2 = [1,2,5,6,7,11,14]
 
 new_list = merge_list(list1,list2)
 
-print(new_list)
+print('merged lists --> ', new_list)
 
 # return first missing positive integer in a list/array
 def firstMissingPositive(A):
@@ -76,7 +76,7 @@ def firstMissingPositive(A):
                     n+=1
         return n
 
-print(firstMissingPositive([1,2,3,4,5,7,8,9]))
+print('first missing positive integer --> ', firstMissingPositive([1,2,3,4,5,7,8,9]))
 
 # return duplicate in list/array in O(N) time
 def repeatedNumber(A):
@@ -92,7 +92,7 @@ def repeatedNumber(A):
         #     if(A.count(x) > 1):
         #         return x
 
-print(repeatedNumber([1,3,4,5,8,10,24,12,5]))
+print('first duplicate in a list --> ', repeatedNumber([1,3,4,5,8,10,24,12,5]))
 
 def max_value(num):
     max = num[0]
@@ -101,7 +101,7 @@ def max_value(num):
             max = x
     return max
 
-print(max_value([4,7,9,3,40,23,1,5]))
+print('max value in list --> ', max_value([4,7,9,3,40,23,1,5]))
 
 # given a non negative list of numbers, add 1 to the number and return it as a list
 # ex input [1,2,3,4] ex  [2,3,9]
@@ -115,7 +115,7 @@ def plusOne(A):
         ret_list.append(int(char))
     #return list(add_one)
     return ret_list
-print(plusOne([1,3,4,5,6]))
+print('add one to list of numbers --> ', plusOne([1,3,4,5,6]))
 
 # return input list as a wave like list, where a1 >= a2 <= a3 >= a4 <= a5
 def wave(A):
@@ -126,7 +126,7 @@ def wave(A):
         n+=2
     return A
 
-print(wave([4,3,8,9,7]))
+print('list as wave --> ', wave([4,3,8,9,7]))
 
 str1 = 'python'
 ret_string = ''
@@ -146,8 +146,9 @@ def maxSubArray(A):
     return final_max
 
 arr1 = [-120, -202, -293, -60, -261, -67, 10, 82, -334, -393, -428, -182, -138, -167, -465, -347, -39, -51, -61, -491, -216, -36, -281, -361, -271, -368, -122, -114, -53, -488, -327, -182, -221, -381, -431, -161, -59, -494, -406, -298, -268, -425, -88, -320, -371, -5, 36, 89, -194, -140, -278, -65, -38, -144, -407, -235, -426, -219, 62, -299, 1, -454, -247, -146, 24, 2, -59, -389, -77, -19, -311, 18, -442, -186, -334, 41, -84, 21, -100, 65, -491, 94, -346, -412, -371, 89, -56, -365, -249, -454, -226, -473, 91, -412, -30, -248, -36, -95, -395, -74, -432, 47, -259, -474, -409, -429, -215, -102, -63, 80, 65, 63, -452, -462, -449, 87, -319, -156, -82, 30, -102, 68, -472, -463, -212, -267]
-print(maxSubArray(arr1))
+print('max sum contiguous subarray --> ', maxSubArray(arr1))
 
+# max set
 def maxset(A):
     sets = {}
     curr_set = []
@@ -175,7 +176,7 @@ def maxset(A):
             return sets[key]
 
 
-print(maxset([1,4,5,-4,2,4,-3,1,3]))
+print('max set --> ', maxset([1,4,5,-4,2,4,-3,1,3]))
 
 #return intersection of two lists, return type is a list
 listex = ['a','b','z']
@@ -185,10 +186,36 @@ def intersection(a,b):
     set2 = set(b)
     set3 = set1 & set2
     return list(set3)
-print(intersection(listex, listp))
-
 
 # or do 
 def intersection2(a,b):
     return [val for val in set(a) & set(b)]
-print(intersection2(listex, listp))
+print('intersection of two lists --> ', intersection2(listex, listp))
+
+# given a string return a dict with key as letter and value as count
+# of that letter in the string 
+def multiple_letter_count(word):
+    return {key: word.count(key) for key in word}
+print('multiple letter count --> ', multiple_letter_count('awesome'))
+
+#check if palindrome
+def is_palindrome(word):
+    word.replace(' ', '')
+    rev_str = ''
+    for x in range(len(word), 0, -1):
+        rev_str += word[x - 1]
+    if(rev_str == word):
+        return True
+    return False
+
+#check if palindrome
+def is_palindrome1(word):
+    strip = word.replace(' ', '')
+    return strip == strip[::-1]
+print('palindrome --> ', is_palindrome1('tacocat'))
+
+# capitalize first letter in string
+def capitalize(string):
+    return string[:1].upper() + string[1:]
+    # or
+    # return word.replace(word[0], word[0].upper())
