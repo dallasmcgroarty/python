d79a545e4a2f6c6b4a4970268a269c2015c02119
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2019-11-10 14:31:24 -0800

    switching code around

--- pract.py
+++ pract.py
@@ -414,3 +414,19 @@ def maximumToys(prices, k):
     return max_toys
 
 print('Max num toys ==> ', maximumToys([14,2,15,18,25,36,4,6,21], 100))
+
+# given list of expenditures and number or days
+# notifications will sent if number of days is up
+# and the current expenditure is greater of equal to
+# the median of the trailing days * 2 
+# return num of notifications left
+
+# apparently not fast enough for large inputs
+def activityNotifications(expenditure, d):
+    #prev = expenditure[0:d]
+    notifs = 0
+    for i in range(d, len(expenditure)):
+        median = statistics.median(expenditure[i-d:i])
+        if expenditure[i] >= 2 * median:
+            notifs += 1
+    return notifs
