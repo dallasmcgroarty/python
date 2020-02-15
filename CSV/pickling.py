380a6ee1c4a9ebe90a75fc8e51f2603601cac01c
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-02-14 17:23:33 -0800

    pickling

---
+++ CSV/pickling.py
@@ -0,0 +1,36 @@
+import pickle
+# pickle lets you save data or objects into a file that can be later loaded and used
+class Animal:
+	def __init__(self, name, species):
+		self.name = name
+		self.species = species
+
+	def __repr__(self):
+		return f"{self.name} is a {self.species}"
+
+	def make_sound(self, sound):
+		print(f"this animal says {sound}")
+
+
+class Cat(Animal):
+	def __init__(self, name, breed, toy):
+		super().__init__(name, species="Cat") # Call init on parent class
+		self.breed = breed
+		self.toy = toy
+
+	def play(self):
+		print(f"{self.name} plays with {self.toy}")
+
+
+blue = Cat("Blue", "Scottish Fold", "String")
+
+# To pickle an object:
+# wb flag to write binary
+with open("pets.pickle", "wb") as file:
+	pickle.dump(blue, file)
+
+# To unpickle something:
+# with open("pets.pickle", "rb") as file:
+# 	zombie_blue = pickle.load(file)
+# 	print(zombie_blue)
+# 	print(zombie_blue.play())
