462ae8b50e93405f61fa4306c2eb96dd96527a75
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-03-06 09:32:48 -0800

    pract

--- coding_practice/practice.py
+++ coding_practice/practice.py
@@ -457,4 +457,14 @@ def split_strings(s):
 
 print(split_strings('adfffggrhra'))
 
+print('--- Fibonacci ---', inspect.getframeinfo(inspect.currentframe()).lineno)
+# given the nth fib number, print that fibonacci number
+def nth_fib(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return nth_fib(n-1) + nth_fib(n-2)
+
 
