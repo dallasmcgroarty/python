e377d5fba2c4d86cd453052482a4c0e42dd0c57f
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-03-10 22:33:42 -0700

    test

---
+++ coding_practice/testing.py
@@ -0,0 +1,26 @@
+d = {'Thomas':1,'Bobby':2,'Carlos':3}
+
+for i in d:
+    print (i)
+
+str1 = 'Hey its me George Bush'
+s = str1.split(' ')
+print(s)
+p = str1.find('George Buh')
+print(p)
+# i = p + len('George Bush')-1
+# print('at index', p)
+# print(len(str1))
+print(i)
+
+if 'George' in str1:
+    print('found!')
+
+str2 = '123'
+print(len(str2))
+name = 'John Adams'
+print(len(name))
+
+m = 'George H. W. Bush'
+dd = m.split(' ')
+print(dd[-1])
