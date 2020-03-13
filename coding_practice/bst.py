f50c9851e4cb13d568b02cb0574a0e3eec964f3f
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-03-12 19:26:06 -0700

    bst pract

---
+++ coding_practice/bst.py
@@ -0,0 +1,89 @@
+class Node:
+    def __init__(self, val):
+        self.value = val
+        self.leftChild = None
+        self.rightChild = None
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+    
+    def __len__(self):
+        return self.size
+    
+    def length(self):
+        return self.size
+    
+    def addNode(self, value):
+        if self.root == None:
+            self.root = Node(value)
+            self.size += 1
+        else:
+            temp = self.root
+            while temp:
+                if value < temp.value:
+                    if temp.leftChild == None:
+                        temp.leftChild = Node(value)
+                        self.size += 1
+                        return
+                    temp = temp.leftChild
+                elif value > temp.value:
+                    if temp.rightChild == None:
+                        temp.rightChild = Node(value)
+                        self.size += 1
+                        return
+                    temp = temp.rightChild
+        
+    def find(self, value):
+        temp = self.root
+        while temp:
+            if value == temp.value:
+                return True
+            elif value < temp.value:
+                temp = temp.leftChild
+            else:
+                temp = temp.rightChild
+        return False
+    
+    def min(self):
+        temp = self.root
+        while temp:
+            if temp.leftChild == None:
+                return temp.value
+            else:
+                temp = temp.leftChild
+    
+    def max(self):
+        temp = self.root
+        while temp:
+            if temp.rightChild == None:
+                return temp.value
+            else:
+                temp = temp.rightChild
+    
+    def height(self):
+        pass
+
+
+    def inorder(self,t):
+        if t.leftChild:
+            self.inorder(t.leftChild)
+        print(t.value)
+        if t.rightChild:
+            self.inorder(t.rightChild)
+   
+
+t = BST()
+t.addNode(5)
+t.addNode(6)
+t.addNode(4)
+t.addNode(7)
+t.addNode(3)
+t.addNode(2)
+t.inorder(t.root)
+print(t.find(1))
+print('min value in tree -> ', t.min())
+print('max value in tree -> ', t.max())
+print('size of tree is -> ', len(t))
--- coding_practice/testing.py
+++ coding_practice/testing.py
@@ -6,8 +6,9 @@ for i in d:
 str1 = 'Hey its me George Bush'
 s = str1.split(' ')
 print(s)
-p = str1.find('George Buh')
-print(p)
+p = str1.find('George Bush')
+x = p + len('George Bush')
+print(p,x)
 # i = p + len('George Bush')-1
 # print('at index', p)
 # print(len(str1))
@@ -24,3 +25,19 @@ print(len(name))
 m = 'George H. W. Bush'
 dd = m.split(' ')
 print(dd[-1])
+
+info = (1,'fff','fff')
+print(info)
+
+a = []
+a.append(info)
+print(a)
+
+str3 = 'When was Richard Nixon president?'
+start = str3.find('Richard Nixon')
+end = start + len('Richard Nixon')
+print(start,end)
+
+presidents = {'George Washington':1, 'John Adams':2, 'Thomas Jefferson':3, 'James Madison':4, 'James Monroe':5, 'John Quincy Adams':6, 'Andrew Jackson':7, 'Martin Van Buren':8, 'William Henry Harrison':9, 'John Tyler':10, 'James K. Polk':11, 'Zachary Taylor':12, 'Millard Fillmore':13, 'Franklin Pierce':14, 'James Buchanan':15, 'Abraham Lincoln':16, 'Andrew Johnson':17, 'Ulysses S. Grant':18, 'Rutherford B. Hayes':19, 'James A. Garfield':20, 'Chester A. Arthur':21, 'Grover Cleveland':22, 'Benjamin Harrison':23, 'Grover Cleveland (2nd term)':24, 'William McKinley':25, 'Theodore Roosevelt':26, 'William Howard Taft':27, 'Woodrow Wilson':28, 'Warren G. Harding':29, 'Calvin Coolidge':30, 'Herbert Hoover':31, 'Franklin D. Roosevelt':32, 'Harry S. Truman':33, 'Dwight D. Eisenhower':34, 'John F. Kennedy':35, 'Lyndon B. Johnson':36, 'Richard Nixon':37, 'Gerald Ford':38, 'Jimmy Carter':39, 'Ronald Reagan':40, 'George H. W. Bush':41, 'Bill Clinton':42, 'George W. Bush':43, 'Barack Obama':44, 'Donald Trump':45}
+for i in presidents:
+    print(i)    
