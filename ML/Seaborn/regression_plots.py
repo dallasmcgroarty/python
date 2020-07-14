81c6daf12e457336b2fe9170b1d978e27576db96
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-07-14 12:54:54 -0700

    regression plot/lm

---
+++ ML/Seaborn/regression_plots.py
@@ -0,0 +1,17 @@
+# regression plotttsss
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+tips = sns.load_dataset('tips')
+print(tips.head())
+
+# lmplot
+# displaying linear models
+
+sns.lmplot(x='total_bill',y='tip',data=tips,hue='sex',markers=['o','v'],
+            scatter_kws={'s':100})
+#plt.show()
+
+sns.lmplot(x='total_bill',y='tip',data=tips,col='day',hue='sex',
+            aspect=0.6,size=8)
+plt.show()
