30b05ee6c70b29b0af74b4e6ef245391be04c623
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-07-22 10:36:04 -0700

    starting machine learning

---
+++ ML/Intro_ML/supervised_learning.py
@@ -0,0 +1,17 @@
+# *** Supervised Learning ***
+
+# supervised learning algorithms are trained using labeled examples, 
+# such as an input where the desired output is known
+# for example a segment of text could havea category label such as:
+    # spam vs legitimate emails
+    # positive vs negative moview review
+
+
+# how it works:
+    # networks receives a set of inputs along with the corresponding correct outputs,
+    # and the algorithm learns by comparing its actual output with corect outputs
+    # to find errors
+    # it then modifies the model accordingly
+
+# supervised learning is commonly used in applications where historical data
+# predicts likely future events
