2f78f171151d62f3db62e649baa0c65c13dcfb92
Dallas <dallas.mcgroarty@gmail.com>
2024-06-24 10:16:01 -0700

    relarning python everyday

---
+++ 100daysofpy/day1/code.py
@@ -0,0 +1,23 @@
+#printing 
+
+print('test')
+print('test "quote in string" test')
+print('test with \'break\' teest')
+
+print('Hello \nhello again\nhello using backslash n')
+
+print('concat' + 'string')
+
+#input function
+answer = input('a prompt from user\n')
+print(answer)
+
+print(len(input('enter your name')))
+
+# name gen project
+
+print('Hello welcome to name gen!')
+city = input('what city did your grow up in?\n')
+pet = input('What is the name of a pet?\n')
+print('Your band name is ' + city + ' ' + pet + '!')
+
---
+++ 100daysofpy/day2/code.py
@@ -0,0 +1,8 @@
+# python primative types
+# int, string, boolean, float
+
+a = '123'
+print(str(a))
+
+b = 123
+print(int(b))
